5fa0a6d4a75394a316435e4be974fc292aecefdb
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-22 13:54:08 -0600

    Practice Object Basics in JS task one complete

---
+++ task_one.js
@@ -0,0 +1,7 @@
+// Create a variable called myString. Set myString to an object literal
+// that has a property string whose value is "Programming with Treehouse is fun!"
+const myString = {
+    string: "Programming with Treehouse is fun!"
+}
+
+console.log(myString);
